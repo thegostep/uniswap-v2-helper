57062f2da6b1607cb16ab0e8ba32f6ce2837b035
thegostep <thegostep@gmail.com>
2020-05-23 21:53:25 -0400

    add swap helper

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# uniswap-v2-helper
+# Uniswap V2 Helper
+
+Uniswap trades in a single function call.
+
+## Usage
+
+```js
+import { swapTokens } from "uniswap-v2-helper";
+import { ethers } from "ethers";
+
+const privateKey = "0x...";
+const provider = ethers.getDefaultProvider();
+const signer = new ethers.Wallet(privateKey, provider);
+
+const receipt = await swapTokens({
+  ethersSigner: signer,
+  recipientAddress: "0x...",
+  outputAmount: "1",
+  inputTokenAddress: "0x...",
+  outputTokenAddress: "0x...",
+  inputTokenDecimals: 6,
+  outputTokenDecimals: 18,
+  maxSlippage: 5,
+  maxDelay: 60 * 2
+});
+```
---
+++ src/index.js
@@ -0,0 +1,153 @@
+const {
+  ChainId,
+  Token,
+  TokenAmount,
+  Pair,
+  Percent,
+  TradeType,
+  Route,
+  Trade
+} = require("@uniswap/sdk");
+
+const { ethers } = require("ethers");
+const IUniswapV2ERC20 = require("@uniswap/v2-core/build/IUniswapV2ERC20.json");
+const IUniswapV2Pair = require("@uniswap/v2-core/build/IUniswapV2Pair.json");
+const IUniswapV2Factory = require("@uniswap/v2-core/build/IUniswapV2Factory.json");
+const IUniswapV2Router01 = require("@uniswap/v2-periphery/build/IUniswapV2Router01.json");
+
+async function swapTokens({
+  ethersSigner,
+  recipientAddress,
+  outputAmount,
+  inputTokenAddress,
+  outputTokenAddress,
+  inputTokenDecimals = 18,
+  outputTokenDecimals = 18,
+  maxSlippage = 5,
+  maxDelay = 60 * 2
+}) {
+  const provider = ethersSigner.provider;
+
+  recipientAddress = ethers.utils.getAddress(recipientAddress);
+  inputTokenAddress = ethers.utils.getAddress(inputTokenAddress);
+  outputTokenAddress = ethers.utils.getAddress(outputTokenAddress);
+  outputAmount = ethers.utils.parseUnits(outputAmount, outputTokenDecimals);
+  maxSlippage = new Percent(maxSlippage);
+
+  const UniswapRouterContract = new ethers.Contract(
+    "0xf164fC0Ec4E93095b804a4795bBe1e041497b92a",
+    IUniswapV2Router01.abi,
+    ethersSigner
+  );
+
+  const uniswapFactoryAddress = ethers.utils.getAddress(
+    await UniswapRouterContract.factory()
+  );
+
+  const UniswapFactoryContract = new ethers.Contract(
+    uniswapFactoryAddress,
+    IUniswapV2Factory.abi,
+    ethersSigner
+  );
+
+  const uniswapPairAddress = ethers.utils.getAddress(
+    await UniswapFactoryContract.getPair(inputTokenAddress, outputTokenAddress)
+  );
+
+  const UniswapPairContract = new ethers.Contract(
+    uniswapPairAddress,
+    IUniswapV2Pair.abi,
+    ethersSigner
+  );
+
+  const token0 = ethers.utils.getAddress(await UniswapPairContract.token0());
+  const token0IsOutput = token0 === outputTokenAddress;
+  const { reserve0, reserve1 } = await UniswapPairContract.getReserves();
+  const inputReserve = token0IsOutput ? reserve1 : reserve0;
+  const outputReserve = token0IsOutput ? reserve0 : reserve1;
+
+  const UniswapInputToken = new Token(
+    ChainId.GOERLI,
+    inputTokenAddress,
+    inputTokenDecimals
+  );
+  const UniswapOutputToken = new Token(
+    ChainId.GOERLI,
+    outputTokenAddress,
+    outputTokenDecimals
+  );
+
+  const UniswapPair = new Pair(
+    new TokenAmount(UniswapInputToken, inputReserve),
+    new TokenAmount(UniswapOutputToken, outputReserve)
+  );
+
+  const UniswapRoute = new Route([UniswapPair], UniswapInputToken);
+
+  const ExactOutputAmount = new TokenAmount(UniswapOutputToken, outputAmount);
+  const ExactOutputAmountFormatted = ethers.utils.parseUnits(
+    ExactOutputAmount.toFixed(),
+    outputTokenDecimals
+  );
+
+  const UniswapTrade = new Trade(
+    UniswapRoute,
+    ExactOutputAmount,
+    TradeType.EXACT_OUTPUT
+  );
+
+  const MaxInputAmount = await UniswapTrade.maximumAmountIn(maxSlippage);
+  const MaxInputAmountFormatted = ethers.utils.parseUnits(
+    MaxInputAmount.toFixed(),
+    inputTokenDecimals
+  );
+
+  const currentTimestamp = (await provider.getBlock()).timestamp;
+  const deadline = currentTimestamp + maxDelay;
+
+  const InputTokenContract = new ethers.Contract(
+    inputTokenAddress,
+    IUniswapV2ERC20.abi,
+    ethersSigner
+  );
+
+  const InputTokenBalance = await InputTokenContract.balanceOf(
+    ethersSigner.getAddress()
+  );
+
+  if (InputTokenBalance.lt(MaxInputAmountFormatted)) {
+    throw new Error("insufficient balance");
+  }
+
+  const allowance = await InputTokenContract.allowance(
+    ethersSigner.getAddress(),
+    UniswapRouterContract.address
+  );
+
+  if (allowance.lt(MaxInputAmountFormatted)) {
+    const approveTx = await (
+      await InputTokenContract.approve(
+        UniswapRouterContract.address,
+        MaxInputAmountFormatted
+      )
+    ).wait();
+  }
+
+  const RouteArray = UniswapRoute.path.map(token => token.address);
+
+  const swapTx = await (
+    await UniswapRouterContract.swapTokensForExactTokens(
+      ExactOutputAmountFormatted,
+      MaxInputAmountFormatted,
+      RouteArray,
+      recipientAddress,
+      deadline
+    )
+  ).wait();
+
+  return swapTx;
+}
+
+module.exports = {
+  swapTokens
+};
